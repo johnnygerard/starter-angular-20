fff9598818bb96c7fba21b4a7b5486cbe2900fbf
Johnny Gérard <john@jgerard.dev>
2025-06-08 01:52:03 +0200

    feat: reset app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styles: [],
+  host: { class: "block" },
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class App {
-  protected title = 'starter-angular-20';
-}
+export class App {}
