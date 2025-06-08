4bd816e99e96c1abbcbfd6aeaeb2711764db2225
Johnny Gérard <john@jgerard.dev>
2025-06-08 02:03:52 +0200

    feat: add home and not found pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    loadComponent: async () => (await import("@/app/page/home/home")).Home,
+    title: "Home",
+  },
+  {
+    path: "**",
+    loadComponent: async () => (await import("@/app/page/not-found/not-found")).NotFound,
+    title: "Page Not Found",
+  },
+];
---
+++ src/app/page/home/home.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.ng.html',
+  host: { class: "block" },
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class Home {
+
+}
